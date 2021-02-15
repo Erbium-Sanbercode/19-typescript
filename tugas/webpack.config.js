4eadadb9417d877b57d640ad03e927cc66a8cd4a
Inosensius Karelo Hesay <inosensius241099@gmail.com>
2021-02-15 20:20:39 +0700

    little change in webpack config

--- tugas/webpack.config.js
+++ tugas/webpack.config.js
@@ -1,33 +1,26 @@
-import { resolve } from 'path';
+import path from 'path';
 
-export const entry = {
-  tasks: './webapp/src/tasks/main.js',
-  worker: './webapp/src/worker/main.js',
-  performance: './webapp/src/performance/main.js',
-};
-export const output = {
-  path: resolve(__dirname, 'www'),
-  filename: '[name].js',
-};
-export const devtool = 'inline-source-map';
-export const devServer = {
-  contentBase: './webapp//www',
-  port: 7000,
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.yaml$/,
-      use: [{ loader: 'json-loader' }, { loader: 'yaml-loader' }],
-    },
-    {
-      test: /\.ts$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/,
-    },
-  ],
+module.exports = {
+  entry: {
+    tasks: './webapp/src/tasks/main.js',
+    worker: './webapp/src/worker/main.js',
+    performance: './webapp/src/performance/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'www'),
+    filename: '[name].js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './webapp//www',
+    port: 7000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
