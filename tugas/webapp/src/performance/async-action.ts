b6681480568dd311e3715851f527172a40d51252
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-02-21 23:16:00 +0700

    convert webapp/performance code to Typescript

--- tugas/webapp/src/performance/async-action.ts
+++ tugas/webapp/src/performance/async-action.ts
@@ -1,7 +1,8 @@
 import { loadingAction, errorAction, summaryLoadedAction } from './store';
 import * as perfSvc from './performance.client';
+import { Dispatch } from '@reduxjs/toolkit';
 
-export const summary = async (dispatch) => {
+export const summary = async (dispatch: Dispatch): Promise<void> => {
   dispatch(loadingAction());
   try {
     const summary = await perfSvc.summary();
--- tugas/webapp/src/performance/config.ts
+++ tugas/webapp/src/performance/config.ts
@@ -1,2 +1,1 @@
-export const SERVICE_BASEURL =
-  process.env.PERFORMANCE_SERVICE_BASEURL || 'http://localhost:7003';
+export const SERVICE_BASEURL = 'http://localhost:7003';
